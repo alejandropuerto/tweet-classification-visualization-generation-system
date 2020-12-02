0541398b83a8b17e6f57d18f7625632face0cfae
Alejandro Puerto Castro <puertoalejandro@outlook.com>
2020-12-01 19:47:17 -0600

    add graphics

--- visualizations/script.js
+++ visualizations/script.js
@@ -1,11 +1,13 @@
-function realTimeLineChart() {
-  var margin = {top: 20, right: 20, bottom: 20, left: 20},
-      width = 600,
-      height = 400,
-      duration = 500,
-      color = d3.schemeCategory10;
+<!-- First visualization -->
 
-  function chart(selection) {
+function histogram() {
+    let margin = {top: 10, right: 30, bottom: 30, left: 40},
+        width = 600,
+        height = 400,
+        duration = 500,
+        color = d3.schemeCategory10;
+
+  function histogram_chart(selection) {
     // Based on https://bl.ocks.org/mbostock/3884955
     selection.each(function(data) {
       data = ["x", "y", "z"].map(function(c) {
@@ -33,8 +35,12 @@ function realTimeLineChart() {
         d3.max(data, function(c) { return d3.max(c.values, function(d) { return d.value; })})
       ]);
       z.domain(data.map(function(c) { return c.label; }));
-
-      var line = d3.line()
+    
+        
+    let histogram = d3.histogram()
+        .value(function(d) { return +d.value; })   // I need to give the vector of value
+        .domain(x.domain())  // then the domain of the graphic
+        .thresholds(x.ticks(40)) // then the numbers of bins
         .curve(d3.curveBasis)
         .x(function(d) { return x(d.time); })
         .y(function(d) { return y(d.value); });
@@ -120,35 +126,206 @@ function realTimeLineChart() {
     });
   }
 
-  chart.margin = function(_) {
+  histogram_chart.margin = function(_) {
     if (!arguments.length) return margin;
     margin = _;
-    return chart;
+    return histogram_chart;
   };
 
-  chart.width = function(_) {
+  histogram_chart.width = function(_) {
     if (!arguments.length) return width;
     width = _;
-    return chart;
+    return histogram_chart;
   };
 
-  chart.height = function(_) {
+  histogram_chart.height = function(_) {
     if (!arguments.length) return height;
     height = _;
-    return chart;
+    return histogram_chart;
   };
 
-  chart.color = function(_) {
+  histogram_chart.color = function(_) {
     if (!arguments.length) return color;
     color = _;
-    return chart;
+    return histogram_chart;
   };
 
-  chart.duration = function(_) {
+  histogram_chart.duration = function(_) {
     if (!arguments.length) return duration;
     duration = _;
-    return chart;
+    return histogram_chart;
   };
 
-  return chart;
+  return histogram_chart;
+}
+
+<!-- Second visualization -->
+
+
+
+<!-- Third visualization -->
+
+function distrubition_area() {
+    let margin = {top: 30, right: 30, bottom: 30, left: 50},
+        width = 460 - margin.left - margin.right,
+        height = 400 - margin.top - margin.bottom,
+        duration = 500,
+        color = d3.schemeCategory10;
+
+  function area_chart(selection) {
+    // Based on https://bl.ocks.org/mbostock/3884955
+    selection.each(function(data) {
+      data = ["x", "y", "z"].map(function(c) {
+        return {
+          label: c,
+          values: data.map(function(d) {
+            return {time: +d.time, value: d[c]};
+          })
+        };
+      });
+
+      var t = d3.transition().duration(duration).ease(d3.easeLinear),
+          x = d3.scaleTime().rangeRound([0, width-margin.left-margin.right]),
+          y = d3.scaleLinear().rangeRound([height-margin.top-margin.bottom, 0]),
+          z = d3.scaleOrdinal(color);
+
+      var xMin = d3.min(data, function(c) { return d3.min(c.values, function(d) { return d.time; })});
+      var xMax = new Date(new Date(d3.max(data, function(c) {
+        return d3.max(c.values, function(d) { return d.time; })
+      })).getTime() - (duration*2));
+
+      x.domain([xMin, xMax]);
+      y.domain([
+        d3.min(data, function(c) { return d3.min(c.values, function(d) { return d.value; })}),
+        d3.max(data, function(c) { return d3.max(c.values, function(d) { return d.value; })})
+      ]);
+      z.domain(data.map(function(c) { return c.label; }));
+    
+        
+    let histogram = d3.histogram()
+        .value(function(d) { return +d.value; })   // I need to give the vector of value
+        .domain(x.domain())  // then the domain of the graphic
+        .thresholds(x.ticks(40)) // then the numbers of bins
+        .curve(d3.curveBasis)
+        .x(function(d) { return x(d.time); })
+        .y(function(d) { return y(d.value); });
+
+      var svg = d3.select(this).selectAll("svg").data([data]);
+      var gEnter = svg.enter().append("svg").append("g");
+        gEnter.append("svg")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+        gEnter.append("g")
+            .attr("transform","translate(" + margin.left + "," + margin.top + ")");
+        
+        gEnter.append("g").attr("class", "axis x");
+        gEnter.append("g").attr("class", "axis y");
+        gEnter.append("defs").append("clipPath")
+          .attr("id", "clip")
+            .append("rect")
+          .attr("width", width-margin.left-margin.right)
+          .attr("height", height-margin.top-margin.bottom);
+        gEnter.append("g")
+            .attr("class", "lines")
+            .attr("clip-path", "url(#clip)")
+            .selectAll(".data").data(data).enter()
+            .append("path")
+            .attr("class", "data");
+
+      var legendEnter = gEnter.append("g")
+        .attr("class", "legend")
+        .attr("transform", "translate(" + (width-margin.right-margin.left-75) + ",25)");
+      legendEnter.append("rect")
+        .attr("width", 50)
+        .attr("height", 75)
+        .attr("fill", "#ffffff")
+        .attr("fill-opacity", 0.7);
+      legendEnter.selectAll("text")
+        .data(data).enter()
+        .append("text")
+          .attr("y", function(d, i) { return (i*20) + 25; })
+          .attr("x", 5)
+          .attr("fill", function(d) { return z(d.label); });
+
+      var svg = selection.select("svg");
+      svg.attr('width', width).attr('height', height);
+      var g = svg.select("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+      g.select("g.axis.x")
+        .attr("transform", "translate(0," + (height-margin.bottom-margin.top) + ")")
+        .transition(t)
+        .call(d3.axisBottom(x).ticks(5));
+      g.select("g.axis.y")
+        .transition(t)
+        .attr("class", "axis y")
+        .call(d3.axisLeft(y));
+
+      g.select("defs clipPath rect")
+        .transition(t)
+        .attr("width", width-margin.left-margin.right)
+        .attr("height", height-margin.top-margin.right);
+
+      g.selectAll("g path.data")
+        .data(data)
+        .style("stroke", function(d) { return z(d.label); })
+        .style("stroke-width", 1)
+        .style("fill", "none")
+        .transition()
+        .duration(duration)
+        .ease(d3.easeLinear)
+        .on("start", tick);
+
+      g.selectAll("g .legend text")
+        .data(data)
+        .text(function(d) {
+          return d.label.toUpperCase() + ": " + d.values[d.values.length-1].value;
+        });
+
+      // For transitions https://bl.ocks.org/mbostock/1642874
+      function tick() {
+        d3.select(this)
+          .attr("d", function(d) { return line(d.values); })
+          .attr("transform", null);
+
+        var xMinLess = new Date(new Date(xMin).getTime() - duration);
+        d3.active(this)
+            .attr("transform", "translate(" + x(xMinLess) + ",0)")
+          .transition()
+            .on("start", tick);
+      }
+    });
+  }
+
+  area_chart.margin = function(_) {
+    if (!arguments.length) return margin;
+    margin = _;
+    return area_chart;
+  };
+
+  area_chart.width = function(_) {
+    if (!arguments.length) return width;
+    width = _;
+    return area_chart;
+  };
+
+  area_chart.height = function(_) {
+    if (!arguments.length) return height;
+    height = _;
+    return area_chart;
+  };
+
+  area_chart.color = function(_) {
+    if (!arguments.length) return color;
+    color = _;
+    return area_chart;
+  };
+
+  area_chart.duration = function(_) {
+    if (!arguments.length) return duration;
+    duration = _;
+    return area_chart;
+  };
+
+  return area_chart;
 }
